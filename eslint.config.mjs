d85b7773b0811e598bbf0141216d918ed35730ee
Sohan <sohannaidu123@gmail.com>
2025-03-26 18:12:15 -0500

    lint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 
 
 export default defineConfig([
@@ -10,5 +9,4 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ]);
