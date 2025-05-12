a2142991e26b77aa4bd3658c360ed4d1c71e47a5
Sohan <sohannaidu123@gmail.com>
2025-05-12 02:40:21 -0500

    redo contact section

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -3,7 +3,7 @@ import Socials from "./Socials";
 export default function Contact() {
     return (
         <section id="contact" className="py-20 px-8 sm:px-20 text-center">
-        <h2 className="text-4xl font-bold pb-16">Contact</h2>
+        <h2 className="text-4xl font-bold pb-8">Contact</h2>
         <Socials />
       </section>
     );
