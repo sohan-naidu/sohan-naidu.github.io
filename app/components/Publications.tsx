1bfeabb0b69132d4dd299f738fd356684654b4ff
Sohan Naidu <sohannaidu123@gmail.com>
2026-03-25 19:22:33 -0500

    remove projects and add new pub

--- app/components/Publications.tsx
+++ app/components/Publications.tsx
@@ -4,28 +4,54 @@ export default function Publications(){
         <h2 className="text-4xl font-bold">Publications</h2>
         <div className="grid grid-cols-1 gap-8 mt-8">
 
-          <div className="card m-auto text-gray-300 w-[clamp(260px,90%,1200px)] max-w-screen-lg hover:brightness-90 transition-all cursor-pointer group bg-gradient-to-tl from-gray-900 to-gray-950 hover:from-gray-800 hover:to-gray-950 border-r-2 border-t-2 border-gray-900 m-4 rounded-lg overflow-hidden relative">
-            <div className="px-8 py-10">
-              <div className="font-bold text-xl pb-8">
-                Exploring Artificial Neural Networks in Virtual Reality: A Unity and Leap Motion-Based Visualization of ONNX Models
+          <div className="[--shadow:rgba(60,64,67,0.3)_0_1px_2px_0,rgba(60,64,67,0.15)_0_2px_6px_2px] w-4/5 h-auto rounded-2xl bg-white [box-shadow:var(--shadow)] max-w-screen-lg m-auto">
+            <div className="flex flex-col pt-9 px-8 pb-10 relative">
+              <h2 className="text-xl font-bold mb-8 text-black">
+                Doremi: Designing a Piano Bot
+              </h2>
+              <p className="text-black tracking-widest">
+                Home piano practice is vital for early music learning, yet it often depends on a child’s intrinsic motivation. This paper introduces DoReMi, a piano peer-bot designed as an expressive, encouraging companion for young beginners. Through animated responses, colorful feedback, and an approachable social presence, DoReMi supports children as they practice and interact with the instrument. We have designed different feedback styles and timing strategies to further shape a child’s perception of the robot, and their motivation to continue learning.
+              </p>
+              <div className="text-black mt-8 mb-12">
+                <p className="font-bold">HRI Companion '26: Companion Proceedings of the 21st ACM/IEEE International Conference on Human-Robot Interaction</p>
               </div>
-              <div className="text-gray-300 tracking-widest">
+              <div className="flex items-center justify-between">
+                <p className="text-black">Published: 16th March 2026</p>
+                <a target="_blank" href="https://dl.acm.org/doi/10.1145/3776734.3794611">
+                  <button className="font-semibold cursor-pointer py-2 px-8 w-max text-sm rounded-lg transition-colors text-white bg-black hover:bg-blue-500" type="button">
+                    Read More
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="[--shadow:rgba(60,64,67,0.3)_0_1px_2px_0,rgba(60,64,67,0.15)_0_2px_6px_2px] w-4/5 h-auto rounded-2xl bg-white [box-shadow:var(--shadow)] max-w-screen-lg m-auto">
+            <div className="flex flex-col pt-9 px-8 pb-10 relative">
+              <h2 className="text-xl font-bold mb-8 text-black">
+                Exploring Artificial Neural Networks in Virtual Reality: A Unity and Leap Motion-Based Visualization of ONNX Models
+              </h2>
+              <p className="text-black tracking-widest">
                 There is a rapidly increasing need for learning resources in the field of Artificial Intelligence aimed at newcomers.
                 To this end, we have attempted to make an intuitive visualization of Artificial Neural Networks in Virtual Reality,
                 while trying to make it as general and extensible as possible to allow for future work to build upon it to create a
                 series of highly intuitive and interactive learning resources. Our implementation results in a framework built using
                 careful design considerations that we think results in a solid building block for work that aims to interactively educate
                 individuals in fields such as Artificial Intelligence and other such algorithms.
+              </p>
+              <div className="text-black mt-8 mb-12">
+                <p className="font-bold">2023 9th International Conference on Virtual Reality (ICVR)</p>
               </div>
-              <div className="text-gray-400 mt-8">
-                <p className="font-bold">Published in IEEE: 2023 9th International Conference on Virtual Reality (ICVR)</p>
-                <a target="_blank" href="https://ieeexplore.ieee.org/document/10169803">View full text here</a>
+              <div className="flex items-center justify-between">
+                <p className="text-black">Published: 12th May 2023</p>
+                <a target="_blank" href="https://ieeexplore.ieee.org/document/10169803">
+                  <button className="font-semibold cursor-pointer py-2 px-8 w-max text-sm rounded-lg transition-colors text-white bg-black hover:bg-blue-500" type="button">
+                    Read More
+                  </button>
+                </a>
               </div>
             </div>
-            <div className="h-2 w-full bg-gradient-to-l via-yellow-500 group-hover:blur-xl blur-2xl m-auto rounded transition-all absolute bottom-0"></div>
-            <div className="h-0.5 group-hover:w-full bg-gradient-to-l via-yellow-950 group-hover:via-yellow-500 w-[90%] m-auto rounded transition-all"></div>
           </div>
-
         </div>
       </section>
 
